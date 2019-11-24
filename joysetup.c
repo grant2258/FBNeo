3328a41f575807282fccec879c631a8f19a3cab9
grant2258 <grant2258@users.noreply.github.com>
2019-11-24 20:06:28 +0000

    clean up on windows machine nano isint the best

--- joysetup.c
+++ joysetup.c
@@ -6,16 +6,16 @@
 FILE * fp;
 
 
-char name[8][6] = {
-                        {'A', 0     },
-                        {'B', 0     },
- 			{'X', 0     },
-			{'Y', 0     },
-			{'L', 'B', 0},
- 			{'R', 'B', 0},
-			{'S', 't', 'a', 'r', 't' ,0 },
-                        {'S', 'e', 'l' ,0 },
+char name[8][6] = 	{'A', 0     },
+					{'B', 0     },
+					{'X', 0     },
+					{'Y', 0     },
+					{'L', 'B', 0},
+					{'R', 'B', 0},
+					{'S', 't', 'a', 'r', 't' ,0 },
+					{'S', 'e', 'l' ,0 },
 };
+
 int joystickmap[4][8] = { -1 } ;
 
 static SDL_Joystick* JoyList[4];
@@ -25,8 +25,8 @@ int state (int buttonCount, SDL_Joystick *joystick)
 {
 	for (int button = 0;  button < buttonCount; button++) 
 	{
-          int    state = SDL_JoystickGetButton(joystick, button);
-                          if (state) return button;
+		int state = SDL_JoystickGetButton(joystick, button);
+		if (state) return button;
 	}
 	return -1;
 }
@@ -38,8 +38,9 @@ void main(void)
 	if (SDL_Init( SDL_INIT_VIDEO | SDL_INIT_JOYSTICK ) < 0)
 	{
 		fprintf(stderr, "Couldn't initialize SDL: %s\n", SDL_GetError());
-	        exit(1);
+		exit(1);
 	}
+
 	joystick_count = SDL_NumJoysticks();
 	printf("%d joysticks were found.\n\n", joystick_count );
 
@@ -51,15 +52,15 @@ void main(void)
 		printf(" %s\n",SDL_JoystickName(i));
 		printf("************************************************\n");
 
-       		printf("*[LB]                                     [RB] *\n");
-	        printf("*                                              *\n");
-	        printf("*     [^]                              [Y]     *\n");
-	        printf("*                                              *\n");
-	        printf("* [<]     [>]   [start] [selct]   [X]   [B]    *\n");
-	        printf("*                                              *\n");
-	        printf("*     [v]                          [A]         *\n");
-	        printf("*                                              *\n");
-    		printf("************************************************\n\n");
+		printf("*[LB]                                     [RB] *\n");
+		printf("*                                              *\n");
+		printf("*     [^]                              [Y]     *\n");
+		printf("*                                              *\n");
+		printf("* [<]     [>]   [start] [selct]   [X]   [B]    *\n");
+		printf("*                                              *\n");
+		printf("*     [v]                          [A]         *\n");
+		printf("*                                              *\n");
+		printf("************************************************\n\n");
 
 		SDL_Joystick *joystick = JoyList[i];
 		SDL_JoystickEventState(SDL_IGNORE);
@@ -67,26 +68,26 @@ void main(void)
 		int counter =0;
 		int ask_for_button=1;
 		
-//		FILE * fp;
 		fp = fopen(SDL_JoystickName(i),"w");
-		while (counter < 8 ) // use a while loop for a change :)
 
+		while (counter < 8 ) // use a while loop for a change :)
 		{
 			SDL_PumpEvents();
-        		SDL_JoystickUpdate();
+			SDL_JoystickUpdate();
 			
 			int result;
 		
 			if ( ask_for_button )
 			{
 		
-			printf("please press a button for %s \n",name[counter]); 
-				 ask_for_button = 0;
+				printf("please press a button for %s \n",name[counter]); 
+				ask_for_button = 0;
 			}
 			result = state(buttonCount,joystick);
 			
 			if (result != -1)  
-			{	printf("button %d selected\n",result);
+			{
+				printf("button %d selected\n",result);
 				fprintf(fp, "BUTTON%d = %d\n",counter, result);  
 				counter ++;
 				ask_for_button =1;
@@ -95,5 +96,5 @@ void main(void)
 		}
 		fclose(fp);	
 	}
-}
+};
 
